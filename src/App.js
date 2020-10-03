7c0e547fb404a7f0a701583b5a004ded428cdcb3
Bruno Paulino <brunojppb@gmail.com>
2020-10-03 21:20:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React para Iniciantes</h1>
+      <h2>Aprenda a criar apps React do zero</h2>
+    </div>
+  );
+}
